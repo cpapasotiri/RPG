553e3c7abdc3e4f0d3b1548ab0ff4aa05dd78f42
cpapasotiri <nina.papasotiri@gmail.com>
2021-01-04 00:09:55 +0200

    classes definitions

---
+++ classes.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstring>
+
+#include "classes.hpp"
+
+using namespace std;
+
---
+++ classes.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// classes definitions
+class Item;
+class Weapons;
+class Armor;
+class Potion;
+
+class Spell;
+class IceSpell;
+class FireSpell;
+class LightSpell;
+
+class Living;
+class Hero;
+class Warrior;
+class Sorcerer;
+class Paladin;
+class Monster;
+class Dragon;
+class Exoskeleton;
+class Spirit;
+
+
+
