c87f7b3579a0f00f5cc22c13de6dabfd9292d94f
cpapasotiri <nina.papasotiri@gmail.com>
2021-01-04 05:49:21 +0200

    Constructors/Destructors Living(Hero, Monster)

--- classes.cpp
+++ classes.cpp
@@ -5,3 +5,143 @@
 
 using namespace std;
 
+Living::Living(string n, int l, int h){
+    cout << "A New Living has been created! " << endl;
+    name = n;
+    level = l;
+    healthPower = h;
+}
+
+Living::~Living(){
+    cout << "A Living to be destroyed! " << endl;
+    cout << "name: " << name << " level: " << level << " healthPower: " << healthPower << endl; 
+}
+
+// void Living::changeHelthPower(int number){
+//     if(healthPower == 0)
+// }
+
+string Living::getName()const{
+    return name;
+}
+
+int Living::getLevel()const{
+    return level;
+}
+
+int Living::getHelthPower()const{
+    return healthPower;
+}
+
+
+Hero::Hero(string n, int l, int h, int mp, int s, int d, int a, int mo, int e)
+: Living(n, l, h){
+    cout << "A New Hero has been created! " << endl;
+    magicPower = mp;
+    strenght = s;
+    dexterity = d;
+    agility = a;
+    money = mo;
+    experience = e;
+}
+
+Hero::~Hero(){
+    cout << "A Hero to be destroyed! " << endl;
+}
+
+int Hero::getMagicPower()const{
+    return magicPower;
+}
+
+int Hero::getStrenght()const{
+    return strenght;
+}
+
+int Hero::getDexterity()const{
+    return dexterity;
+}
+
+int Hero::getAgility()const{
+    return agility;
+}
+
+
+Warrior::Warrior(string n, int l, int h, int m, int s, int d, int a)
+: Hero(mn, l, h, m, s, d, a){
+    cout << "A New Warrior has been created! " << endl;
+}
+
+Warrior::~Warrior(){
+    cout << "A Warrior to be destroyed! " << endl;
+}
+
+
+Sorcerer::Sorcerer(int m, int s, int d, int a)
+: Hero(mn, l, h, m, s, d, a){
+    cout << "A New Sorcerer has been created! " << endl;
+}
+
+Sorcerer::~Sorcerer(){
+    cout << "A Sorcerer to be destroyed! " << endl;
+}
+
+
+Paladin::Paladin(int m, int s, int d, int a)
+: Hero(mn, l, h, m, s, d, a){
+    cout << "A New Paladin has been created! " << endl;
+}
+
+Paladin::~Paladin(){
+    cout << "A Paladin to be destroyed! " << endl;
+}
+
+
+
+Monster::Monster(string n, int l, int h, int da, int de)
+: Living(n, h, l){
+    cout << "A New Monster has been created! " << endl;
+    damage = da;
+    defence = de;
+}
+
+Monster::~Monster(){
+    cout << "A Monster to be destroyed! " << endl;
+}
+
+int Monster::getDamage()const{
+    return damage;
+}
+
+int Monster::getDefence()const{
+    return defence;
+}
+
+
+Dragon::Dragon(string n, int l, int h, int da, int de) 
+: Monster(n, l, h, da, de){
+    cout << "A New Dragon has been created! " << endl;
+}
+
+Dragon::~Dragon(){
+    cout << "A Dragon to be destroyed! " << endl;
+}
+
+
+Exoskeleton::Exoskeleton(string n, int l, int h, int da, int de) 
+: Monster(n, l, h, da, de){
+    cout << "A New Exoskeleton has been created! " << endl;
+}
+
+Exoskeleton::~Exoskeleton(){
+    cout << "A Exoskeleton to be destroyed! " << endl;
+}
+
+
+Spirit::Spirit(string n, int l, int h, int da, int de) 
+: Monster(n, l, h, da, de){
+    cout << "A New Spirit has been created! " << endl;
+}
+
+Spirit::~Spirit(){
+    cout << "A Spirit to be destroyed! " << endl;
+}
--- classes.hpp
+++ classes.hpp
@@ -26,3 +26,118 @@ class Spirit;
 
 
 
+class Living{
+    private:
+        string name;
+        int level;
+        int healthPower;
+        
+    public:
+        Living(string, int, int);
+        ~Living();
+
+        void helthCheck();
+        // void changeHelthPower(int);
+
+        string getName()const;
+        int getLevel()const;
+        int getHelthPower()const;
+
+
+};
+
+
+class Hero : public Living{
+    private:
+        int magicPower;
+        int strenght;
+        int dexterity;
+        int agility;
+        int money;
+        int experience;
+        
+    public:
+        Hero(string, int, int, int, int, int, int, int, int);
+        ~Hero();
+
+        int getMagicPower()const;
+        int getStrenght()const;
+        int getDexterity()const;
+        int getAgility()const;
+
+};
+
+
+class Warrior : public Hero{
+    private:
+        
+        
+    public:
+        Warrior(int, int, int, int);
+        ~Warrior();
+};
+
+
+class Sorcerer : public Hero{
+    private:
+        
+        
+    public:
+        Sorcerer(int, int, int, int);
+        ~Sorcerer();
+};
+
+
+class Paladin : public Hero{
+    private:
+        
+        
+    public:
+        Paladin(int, int, int, int);
+        ~Paladin();
+
+};
+
+
+class Monster : public Living{
+    private:
+        int damage;
+        int defence;
+
+    public:
+        Monster(string, int, int, int, int);
+        ~Monster();
+
+        int getDamage()const;
+        int getDefence()const;
+};
+
+
+class Dragon : public Monster{
+    private:
+        
+
+    public:
+        Dragon(string, int, int, int, int);
+        ~Dragon();
+};
+
+
+class Exoskeleton : public Monster{
+    private:
+
+
+    public:
+        Exoskeleton(string, int, int, int, int);
+        ~Exoskeleton();
+};
+
+
+class Spirit : public Monster{
+    private:
+
+
+    public:
+        Spirit(string, int, int, int, int);
+        ~Spirit();
+};
